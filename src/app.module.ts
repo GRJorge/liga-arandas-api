399c22efa0609787dc6634a6210580bbb31d4432
GRJorge <deb.devel@outlook.es>
2025-01-16 23:28:55 -0600

    Connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { env } from 'process';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/' + env.DB_NAME, {
+      onConnectionCreate: (connection: Connection) => {
+        connection.on('connected', () => console.log('Database connected'));
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
