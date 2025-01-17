966d83885481a7e93ac2b0b04cebb2b66f6546b0
GRJorge <deb.devel@outlook.es>
2025-01-17 02:26:00 -0600

    Dto for create admin

---
+++ src/admin/dto/createAdmin.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateAdminDto {
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
