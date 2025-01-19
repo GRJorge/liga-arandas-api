bf3ff022d9439a0aff42d382c09114e9d0e3db3a
GRJorge <deb.devel@outlook.es>
2025-01-18 23:58:31 -0600

    Encrypt password

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,12 +3,13 @@ import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Admin, AdminSchema } from './admin.schema';
+import { HashService } from './hash.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
   ],
-  providers: [AdminService],
+  providers: [AdminService, HashService],
   controllers: [AdminController],
 })
 export class AdminModule {}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -4,15 +4,18 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Admin } from './admin.schema';
 import { EditAdminDto } from './dto/editAdmin.dto';
+import { HashService } from './hash.service';
 
 @Injectable()
 export class AdminService {
   constructor(
     @InjectModel(Admin.name) private readonly adminModel: Model<Admin>,
+    private readonly hashService: HashService,
   ) {}
 
   async createAdmin(admin: CreateAdminDto): Promise<Admin> {
     try {
+      admin.password = await this.hashService.hashPassword(admin.password);
       return await this.adminModel.create(admin);
     } catch (error) {
       if (error.code === 11000) {
