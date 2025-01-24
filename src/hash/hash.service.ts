9b985462381e6960e5175847826a4260f70680cb
GRJorge <deb.devel@outlook.es>
2025-01-24 00:57:04 -0600

    Service in providers

--- src/hash/hash.service.ts
+++ src/hash/hash.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import bcrypt from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 
 @Injectable()
 export class HashService {
   async hash(data: string): Promise<string> {
-    return await bcrypt.hash(data, 10);
+    return await hash(data, 10);
   }
   async compare(plain: string, encrypted: string): Promise<boolean> {
-    return await bcrypt.compare(plain, encrypted);
+    return await compare(plain, encrypted);
   }
 }
