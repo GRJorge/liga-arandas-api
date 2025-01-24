e86c737eecf23f00a751c9ff511ff09826ed195d
GRJorge <deb.devel@outlook.es>
2025-01-24 02:11:39 -0600

    Create and return token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AdminModule } from 'src/admin/admin.module';
 import { HashModule } from 'src/hash/hash.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AdminModule, HashModule],
+  imports: [AdminModule, HashModule, JwtModule.register({ global: true })],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,18 @@ import { LoginDto } from './dto/login.dto';
 import { AdminService } from 'src/admin/admin.service';
 import { Admin } from 'src/admin/admin.schema';
 import { HashService } from 'src/hash/hash.service';
+import { JwtService } from '@nestjs/jwt';
+import { env } from 'process';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly adminService: AdminService,
     private readonly hashService: HashService,
+    private readonly jwtService: JwtService,
   ) {}
 
-  async login(loginData: LoginDto): Promise<string> {
+  async login(loginData: LoginDto): Promise<{ accessToken: string }> {
     const admin: Admin = await this.adminService.getAdminByEmail(loginData.email);
 
     if (!admin) {
@@ -19,7 +22,9 @@ export class AuthService {
     } else if (!(await this.hashService.compare(loginData.password, admin.password))) {
       throw new BadRequestException('Incorrect password');
     } else {
-      return 'Token';
+      const payload = { id: admin._id.toString(), email: loginData.email };
+
+      return { accessToken: await this.jwtService.signAsync(payload, { secret: env.SECRET, expiresIn: loginData.keep ? '30 days' : '1 day' }) };
     }
   }
 }
