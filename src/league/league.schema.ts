75d7630fc4df883454014d41e22338f4c1b8f819
GRJorge <deb.devel@outlook.es>
2025-02-11 01:56:09 -0600

    League schema for db

---
+++ src/league/league.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type LeagueDocument = HydratedDocument<League>;
+
+@Schema({ timestamps: true, versionKey: false })
+export class League {
+  _id: string;
+
+  @Prop({ unique: true })
+  logo: string;
+
+  @Prop({ unique: true })
+  name: string;
+}
+
+export const LeagueSchema = SchemaFactory.createForClass(League);
