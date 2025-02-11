6c3d53216d6d5e986ed7a736ea1801cb3d4daa19
GRJorge <deb.devel@outlook.es>
2025-02-11 02:11:48 -0600

    Import league model

--- src/league/league.module.ts
+++ src/league/league.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LeagueController } from './league.controller';
 import { LeagueService } from './league.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { League, LeagueSchema } from './league.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: League.name, schema: LeagueSchema }])],
   controllers: [LeagueController],
-  providers: [LeagueService]
+  providers: [LeagueService],
 })
 export class LeagueModule {}
