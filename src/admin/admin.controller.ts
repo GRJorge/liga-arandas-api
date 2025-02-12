baaf0da536cc186dfc705e64a2edb9a7d13e6f6b
GRJorge <deb.devel@outlook.es>
2025-02-12 00:19:46 -0600

    Authorization guard in routes

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,22 +1,26 @@
-import { Body, Controller, Delete, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { CreateAdminDto } from './dto/createAdmin.dto';
 import { AdminService } from './admin.service';
 import { EditAdminDto } from './dto/editAdmin.dto';
 import { DeleteAdminDto } from './dto/deleteAdmin.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   async create(@Body() admin: CreateAdminDto) {
     return this.adminService.createAdmin(admin);
   }
   @Patch('/edit')
+  @UseGuards(AuthGuard)
   async edit(@Body() editAdmin: EditAdminDto) {
     return this.adminService.editAdmin(editAdmin);
   }
   @Delete('/delete')
+  @UseGuards(AuthGuard)
   async delete(@Query() id: DeleteAdminDto) {
     return this.adminService.deleteAdmin(id.id);
   }
--- src/league/league.controller.ts
+++ src/league/league.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateLeagueDto } from './dto/createLeague.dto';
 import { LeagueService } from './league.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('league')
 export class LeagueController {
   constructor(private readonly leagueService: LeagueService) {}
 
   @Post('/create')
+  @UseGuards(AuthGuard)
   async create(@Body() league: CreateLeagueDto) {
     return this.leagueService.createLeague(league);
   }
