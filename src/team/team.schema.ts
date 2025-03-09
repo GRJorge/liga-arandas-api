7614495dc7b7bc2fe6d813563a37ba493b450780
GRJorge <deb.devel@outlook.es>
2025-03-08 19:07:53 -0600

    Team Schema

---
+++ src/team/team.schema.ts
@@ -0,0 +1,26 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
+
+export type TeamDocument = HydratedDocument<Team>;
+
+@Schema({ timestamps: true, versionKey: false })
+export class Team {
+  _id: string;
+
+  @Prop({ unique: true })
+  name: string;
+
+  @Prop({ unique: true })
+  shortName: string;
+
+  @Prop({ unique: true })
+  shield: string;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Category' })
+  category: MongooseSchema.Types.ObjectId;
+
+  @Prop({ default: true })
+  active: boolean;
+}
+
+export const TeamSchema = SchemaFactory.createForClass(Team);
