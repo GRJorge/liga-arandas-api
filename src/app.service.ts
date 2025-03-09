44b78b7425bd6fa8dfa80563f7d80bda03c61cfd
GRJorge <deb.devel@outlook.es>
2025-03-08 19:35:51 -0600

    Function for throw duplicated fields to create documents

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  async createDocumentMiddleware(promise: Promise<any>): Promise<any> {
+    try {
+      return await promise;
+    } catch (error) {
+      if (error.code === 11000) {
+        const duplicateField = Object.keys(error.keyValue)[0];
+        throw new ConflictException(`${duplicateField} is already in use`);
+      }
+      throw InternalServerErrorException;
+    }
+  }
+}
